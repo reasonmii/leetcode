6937b9360ac70a7e6eb84ca5629768791039142a
YUNA <79252563+reasonmii@users.noreply.github.com>
2024-05-14 13:14:04 -0500

    Update M_easy.py

--- M_easy.py
+++ M_easy.py
@@ -54,4 +54,93 @@ class Solution(object):
 
         return rst        
 
+# ======================================================================
+# 20. Valid Parentheses
+# Topic : string, brackets, stack
+# ======================================================================
+
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+        stack = []
+
+        for char in s:
+            if char in "([{":
+                stack.append(char)
+            else:
+                if stack == [] or \
+                    (char == ")" and stack[-1] != "(") or \
+                    (char == "]" and stack[-1] != "[") or \
+                    (char == "}" and stack[-1] != "{"):
+                    return False
+                stack.pop()
+        
+        return not stack # stack is empty
+
+# ======================================================================
+# 21. Merge Two Sorted Lists
+# Topic : LinkedList
+# ======================================================================
+
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+
+        cur = dummy = ListNode()
+        
+        while list1 and list2:
+            if list1.val <= list2.val:
+                cur.next = list1
+                list1, cur = list1.next, list1
+            else:
+                cur.next = list2
+                list2, cur = list2.next, list2
+
+        if list1 or list2:
+            cur.next = list1 if list1 else list2
+
+        return dummy.next
+
+# ======================================================================
+# 28. Find the Index of the First Occurrence in a String
+# Topic : string, index
+# ======================================================================
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+
+        end = len(needle)
+        for start in range(len(haystack)):
+            if haystack[start:start+end] == needle:
+                return start
+        return -1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
