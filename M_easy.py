d18f9999de8d024a5284e2e15815ee41cc205936
YUNA <79252563+reasonmii@users.noreply.github.com>
2024-05-14 19:28:16 -0500

    Update M_easy.py

--- M_easy.py
+++ M_easy.py
@@ -147,7 +147,27 @@ class Solution(object):
 
         return dp[-1]
 
+# ======================================================================
+# 88. Merge Sorted Array
+# Topic : Dynamic Programming
+# ======================================================================
 
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        i = m-1
+        j = n-1
+        k = m+n-1
+
+        while j >= 0:
+            if i >= 0 and nums1[i] >= nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -=1
+
+        return nums1
 
 
 
