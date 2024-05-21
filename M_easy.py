db29db7542b7968afaafe8aef9bc561b7d253b2c
YUNA <79252563+reasonmii@users.noreply.github.com>
2024-05-21 16:53:01 -0400

    Update M_easy.py

--- M_easy.py
+++ M_easy.py
@@ -83,16 +83,17 @@ class Solution(object):
 class Solution(object):
     def mergeTwoLists(self, list1, list2):
 
-        cur = dummy = ListNode()
-        
+        dummy = cur = ListNode(-1)
+
         while list1 and list2:
             if list1.val <= list2.val:
                 cur.next = list1
-                list1, cur = list1.next, list1
+                list1 = list1.next
             else:
                 cur.next = list2
-                list2, cur = list2.next, list2
-
+                list2 = list2.next
+            cur = cur.next
+        
         if list1 or list2:
             cur.next = list1 if list1 else list2
 
