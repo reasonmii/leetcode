d13af3e2b63b631aec3a54cdedde18b6d05b1120
YUNA <79252563+reasonmii@users.noreply.github.com>
2024-05-24 21:18:36 -0400

    Update M_medium.py

--- M_medium.py
+++ M_medium.py
@@ -511,6 +511,8 @@ class Solution(object):
 # 54. Spiral Matrix
 # Topic : matrix, pop, append
 # ======================================================================
+# [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+# [1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10]
 
 class Solution(object):
     def spiralOrder(self, matrix):
