01a2ce0d6cb67c07c06c2bc9dc18581f6ffda50b
YUNA <79252563+reasonmii@users.noreply.github.com>
2024-06-22 14:48:18 -0400

    Update blind75.py

--- blind75.py
+++ blind75.py
@@ -419,14 +419,46 @@ class Solution(object):
 # Graph : 8 questions
 # ========================================================
 
-# 133. Clone Graph (Medium)
+# 133. Clone Graph (Medium) ##
 
-# Course Schedule
-# Pacific Atlantic Water Flow
+# 207. Course Schedule (Medium) ##
+
+# 417. Pacific Atlantic Water Flow (Medium) ##
+
 # 200. Number of Islands (Medium)
+
+class Solution(object):
+    def numIslands(self, grid):
+        
+        if not grid: return 0
+
+        rows, cols = len(grid), len(grid[0])
+
+        def dfs(row, col):
+            if row < 0 or col < 0 or row >= rows or col >= cols or grid[row][col] != '1':
+                return
+            
+            grid[row][col] = '0'
+            dfs(row-1, col)
+            dfs(row+1, col)
+            dfs(row, col-1)
+            dfs(row, col+1)
+
+        cnt = 0
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == '1':
+                    dfs(row, col)
+                    cnt += 1
+
+        return cnt
+
 # Longest Consecutive Sequence
-# Alien Dictionary (Leetcode Premium)
-# Graph Valid Tree (Leetcode Premium)
+
+# Alien Dictionary
+
+# Graph Valid Tree
+
 # Number of Connected Components in an Undirected Graph (Leetcode Premium)
 
 # ========================================================
