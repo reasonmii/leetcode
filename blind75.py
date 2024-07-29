76dd7cdde678de65e1fbbbc18b839ce13432c0ef
YUNA <79252563+reasonmii@users.noreply.github.com>
2024-07-29 13:47:52 -0400

    Update blind75.py

--- blind75.py
+++ blind75.py
@@ -1,7 +1,7 @@
 # https://leetcode.com/discuss/general-discussion/460599/blind-75-leetcode-questions
 # https://hackernoon.com/14-patterns-to-ace-any-coding-interview-question-c5bb3357f6ed
 
-```
+"""
 - Array
 - Binary
 - DP
@@ -12,7 +12,7 @@
 - String
 - Tree
 - Heap
-```
+"""
 
 # ========================================================
 # Array : 10 questions
